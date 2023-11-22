f88618f1b0c2b58a0a6fb2abcb1fcd49819b7a91
sebastian.soplan <sebastian.soplan@zazmic.com>
2023-11-22 11:49:30 -0300

    removing route productivity

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -4,3 +4,16 @@ import NextAuth from "next-auth";
 const handler = NextAuth(nextauthOptions)
 
 export { handler as GET, handler as POST };
+
+// export default async function handler(req: any, res: any) {
+//     const session = await getServerSession(req, res, nextauthOptions)
+
+//     if (!session) {
+//         res.status(401).json({ message: "You must be logged in." });
+//         return;
+//     }
+
+//     return res.json({
+//         message: 'Success',
+//     })
+// }
