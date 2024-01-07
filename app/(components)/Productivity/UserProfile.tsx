6600a9d9ad725c00dbdb630a59524e1bce6ddb41
g-marinho <gabrielmarinhopersonal@gmail.com>
2024-01-07 12:03:59 -0300

    Altercoes da branch sem3 com as atualizações feitas pelo Sebas na master

--- app/(components)/Productivity/UserProfile.tsx
+++ app/(components)/Productivity/UserProfile.tsx
@@ -1,23 +1,25 @@
 import React from "react";
 
-const UserProfile = (user: any) => {
-  const { photo, rankingPosition, name, age, sector, indicators } = user;
+interface User {
+    rankingPosition: number;
+    id: string;
+    indicators: number[];
+}
+
+const UserProfile = (user: User) => {
+  const { rankingPosition, id, indicators } = user;
 
   return (
-    <div className="flex gap-4 mt-4">
-      <img src={photo} alt={`perfil do ${name}`} className="rounded-xl" />
-      <div className="flex flex-col gap-3">
-        <p className="text-4xl font-bold">{rankingPosition}</p>
-        <p>{name}</p>
-        <p>{`${age} anos`}</p>
-        <p>{sector}</p>
+    <div className="flex gap-4 mt-12 ">
+      <div className="flex flex-col gap-3 mr-10">
+        <p className="text-4xl font-bold">{`${rankingPosition}º`}</p>
+        <p className="text-sm">{id}</p>
       </div>
-      <h4>Indicadores</h4>
       {Array.isArray(indicators) && indicators.length > 0 ? (
         indicators.map((indicator, index) => (
-          <div key={index}>
-            <p>Título {index + 1}</p>
-            <span>{indicator}</span>
+          <div key={index} className="flex flex-col">
+            <p className="text-gray-400 text-sm">Título {index + 1}</p>
+            <span className="text-blue-950 text-3xl font-bold">{indicator}</span>
           </div>
         ))
       ) : (
