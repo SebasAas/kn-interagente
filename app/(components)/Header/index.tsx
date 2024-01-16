84f6f07660bb7f0545d7f1a505bd522b099919b9
sebastian.soplan <sebastian.soplan@zazmic.com>
2024-01-16 13:09:00 -0300

    remove opt from header

--- app/(components)/Header/index.tsx
+++ app/(components)/Header/index.tsx
@@ -41,7 +41,7 @@ const Header = ({ activePath }: { activePath: string }) => {
         <div className=" justify-between items-center w-full lg:flex lg:w-auto">
           {!notShowBtnLoginRegisterInPaths.includes(activePath) && user && (
             <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
-              <li>
+              {/* <li>
                 <Link
                   href="/"
                   className="block py-2 pr-4 pl-3 text-white rounded bg-primary-700 lg:bg-transparent lg:text-primary-700 lg:p-0 dark:text-white"
@@ -49,23 +49,24 @@ const Header = ({ activePath }: { activePath: string }) => {
                 >
                   Dashboard
                 </Link>
-              </li>
+              </li> */}
               <li>
                 <Link
                   href="/productivity"
-                  className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
+                  aria-current="page"
+                  className="block py-2 pr-4 pl-3 text-white rounded bg-primary-700 lg:bg-transparent lg:text-primary-700 lg:p-0 dark:text-white"
                 >
                   Produtividade
                 </Link>
               </li>
-              <li>
+              {/* <li>
                 <Link
                   href="/scale"
                   className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
                 >
                   Escala
                 </Link>
-              </li>
+              </li> */}
             </ul>
           )}
         </div>
