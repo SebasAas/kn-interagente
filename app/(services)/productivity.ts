a379fc884572c7d5e653852e8982acc424ef2389
Sebastian <sebastiansoplan@outlook.com>
2024-01-25 11:56:16 -0300

    changing url production

--- app/(services)/productivity.ts
+++ app/(services)/productivity.ts
@@ -1,31 +1,42 @@
 import { toast } from "react-toastify";
 
-export const BASE_URL = "https://producao-kn-emachzhqzq-uc.a.run.app/";
+export const BASE_URL = "https://production-kn-emachzhqzq-uc.a.run.app/";
 
-export const fetchProductionCharts = async (month: string, year: string, shift: string) => {
-    const response = await fetch(`${BASE_URL}production/charts?month=${month}&year=${year}&shift=${shift}`);
-    const data = await response.json();
-    return data;
+export const fetchProductionCharts = async (
+  month: string,
+  year: string,
+  shift: string
+) => {
+  const response = await fetch(
+    `${BASE_URL}production/charts?month=${month}&year=${year}&shift=${shift}`
+  );
+  const data = await response.json();
+  return data;
 };
 
-export const fetchProductionVisits = async (month: string, year: string, shift: string) => {
-    const response = await fetch(`${BASE_URL}production/visits?month=${month}&year=${year}&shift=${shift}`);
-    const data = await response.json();
-    return data;
+export const fetchProductionVisits = async (
+  month: string,
+  year: string,
+  shift: string
+) => {
+  const response = await fetch(
+    `${BASE_URL}production/visits?month=${month}&year=${year}&shift=${shift}`
+  );
+  const data = await response.json();
+  return data;
 };
-
 
 export const uploadFiles = async (files: File[]) => {
-    const formData = new FormData();
+  const formData = new FormData();
 
-    files.forEach(file => {
-        formData.append('files', file);
-    });
+  files.forEach((file) => {
+    formData.append("files", file);
+  });
 
-    const promise = fetch(`${BASE_URL}production/uploadfile`, {
-        method: 'POST',
-        body: formData,
-    });
+  const promise = fetch(`${BASE_URL}production/uploadfile`, {
+    method: "POST",
+    body: formData,
+  });
 
-    return promise
+  return promise;
 };
