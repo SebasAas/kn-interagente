6a84b5aa918d9d5808022ccbea3866f28e470e3b
Sebastian <sebastiansoplan@outlook.com>
2024-02-09 11:44:21 -0300

    changing wss

--- app/(components)/WSS/WebSocketRanking.tsx
+++ app/(components)/WSS/WebSocketRanking.tsx
@@ -11,7 +11,7 @@ export const WebSocketRanking = ({
   setWSSRankingFinished: (status: boolean) => void;
 }) => {
   const [socketUrl, setSocketUrl] = useState(
-    `ws://kn-workers-dev-emachzhqzq-uc.a.run.app/ws/status_ranking`
+    `wss://kn-workers-dev-emachzhqzq-uc.a.run.app/ws/status_ranking`
   );
   const [connectionTrigger, setConnectionTrigger] = useState(0); // Used to trigger reconnection
 
