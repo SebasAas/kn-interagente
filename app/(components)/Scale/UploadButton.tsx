101c19c3163c5783760495c2d7d877fa4e66742d
g-marinho <gabrielmarinhopersonal@gmail.com>
2024-02-22 22:40:45 -0300

    add pagina escala e sua UI com os componentes dateselector, BoxSeparation, AlertBoard e UploadButton

---
+++ app/(components)/Scale/UploadButton.tsx
@@ -0,0 +1,33 @@
+"use client";
+import React, { useState } from 'react';
+
+interface UploadButtonProps {
+  onUploadComplete: () => void;
+}
+
+const UploadButton: React.FC<UploadButtonProps> = ({ onUploadComplete }) => {
+  const [lastUploadTime, setLastUploadTime] = useState<string | null>(null);
+
+  const handleUpload = async () => {
+    await new Promise(resolve => setTimeout(resolve, 2000));
+    const uploadTime = new Date().toLocaleString();
+    setLastUploadTime(uploadTime);
+    onUploadComplete();
+  };
+
+  return (
+    <div className='flex flex-col gap-8 ml-8'>
+      <button
+        className="bg-blue-500 hover:bg-blue-700 text-white text-sm font-bold py-1 px-2 rounded-full w-4/5"
+        onClick={handleUpload}
+      >
+        Carregar Base
+      </button>
+      {lastUploadTime && (
+        <p className="text-black text-sm">Último upload em: {lastUploadTime}</p>
+      )}
+    </div>
+  );
+};
+
+export default UploadButton;
--- app/scale/page.tsx
+++ app/scale/page.tsx
@@ -1,27 +1,49 @@
 "use client";
-import { Divider } from "@nextui-org/react";
+import { CardBody, Divider } from "@nextui-org/react";
 import BoxSeparation from "../(components)/Scale/BoxSeparation";
-import DateSelector from "../(components)/Scale/DateSelector"
-
-import { generateFakeData } from "./dadosmock";
+import DateSelector from "../(components)/Scale/DateSelector";
+import AlertBoard from "../(components)/Scale/AlertBoard";
+import UploadButton from "../(components)/Scale/UploadButton";
 
 const ScalePage: React.FC = () => {
-    const dadosMock = generateFakeData();
-    function handleDateSelect(date: Date) {
-        //Lógica pra carregar dados do back
-        console.log("Carregar dados para:", date);
-    }
-    return (
-        <div>
-            <div className="flex flex-row mt-8 p-4 border-1 border-solid rounded-lg transform rotate-x-2 shadow-md">
-                <DateSelector onDateSelect={handleDateSelect} />
+  function handleDateSelect(date: Date) {
+    //Lógica pra carregar dados do back
+    console.log("Carregar dados para:", date);
+  }
+    const handleUploadComplete = () => {
+        console.log('Upload completo!');
+      };
+
+  return (
+    <div>
+      <div className="flex flex-row mt-8 p-4 border-1 border-solid rounded-lg transform rotate-x-2 shadow-md">
+        <DateSelector onDateSelect={handleDateSelect} />
+      </div>
+      <div className="flex flex-row gap-2">
+        <div className="flex flex-col mt-8 p-4 border-1 border-solid rounded-lg transform rotate-x-2 shadow-md w-5/12 h-96 overflow-auto">
+          <div>
+            <BoxSeparation />
+          </div>
+        </div>
+        <div className="flex flex-col gap-4 mt-8 w-5/12">
+          <div className=" flex flex-row border-1 border-solid rounded-lg transform rotate-x-2 shadow-md h-1/3">
+            <AlertBoard />
+          </div>
+          <div className="border-1 border-solid rounded-lg transform rotate-x-2 shadow-md h-2/3"></div>
+        </div>
+        <div className="flex flex-col gap-4 mt-8 w-2/12">
+            <div className="flex flex-col gap-8 border-1 border-solid rounded-lg transform rotate-x-2 shadow-md h-1/2">
+                <h3 className="mt-4 ml-8">Escala</h3>
+                <UploadButton onUploadComplete={handleUploadComplete}/>
             </div>
-            <div className="flex flex-row mt-8 p-4 border-1 border-solid rounded-lg transform rotate-x-2 shadow-md w-5/12 h-96">
-            <BoxSeparation data={dadosMock} />
-            <Divider />
+            <div className="flex flex-col gap-8 border-1 border-solid rounded-lg transform rotate-x-2 shadow-md h-1/2">
+                <h3 className="mt-4 ml-8">Demanda</h3>
+                <UploadButton onUploadComplete={handleUploadComplete}/>
             </div>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
 export default ScalePage;
