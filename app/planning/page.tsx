795153f9a18f33846239fc0f5a303103e93ea0ef
Sebastian <sebastiansoplan@outlook.com>
2024-08-14 16:21:56 -0300

    chore: Update CSS styles and add new functionality to PlanningPage component

--- app/planning/page.tsx
+++ app/planning/page.tsx
@@ -1,6 +1,13 @@
 "use client";
 import React, { useState, useEffect } from "react";
-import { Card, CardBody, CardHeader, Divider } from "@nextui-org/react";
+import {
+  Card,
+  CardBody,
+  CardHeader,
+  Divider,
+  Input,
+  Switch,
+} from "@nextui-org/react";
 import DateSelector from "../(components)/Planning/DateSelector";
 import AlertBoard from "../(components)/Planning/AlertBoard";
 import UploadButton from "../(components)/Planning/UploadButton";
@@ -28,6 +35,8 @@ const PlanningPage: React.FC = () => {
   const { dispatch, simulation, selectedSimulationDate } = useAppContext();
   const [demandFile, setDemandFile] = useState<File | null>(null);
   const [isVisible, setIsVisible] = useState(false);
+  const [buttonDisabled, setButtonDisabled] = useState(false);
+  const [backlogPrimary, setBacklogPrimary] = useState(false);
 
   const [dateInfoCallback, setDateInfoCallback] = useState({
     year: "2024",
@@ -97,19 +106,36 @@ const PlanningPage: React.FC = () => {
     }
   };
 
+  const handleGetInformation = async () => {
+    console.log("handleGetInformation");
+  };
+
+  const handleGetDataFormat = () => {
+    // Find the obj inside lastUploadFileSummary with name "upload" the type of the data is 2024-07-18T18:00:00, and I want to show like "18:00 - 18/07"
+    // const uploadData = lastUploadFileSummary?.find(
+    //   (data) => data?.name === "upload"
+    // );
+
+    // if (uploadData) {
+    //   const date = new Date(uploadData.day);
+    //   const hours = date.getHours();
+    //   const minutes = date.getMinutes();
+    //   const day = date.getDate();
+    //   const month = date.getMonth() + 1;
+    //   return `${hours}:${minutes} - ${day}/${month}`;
+    // }
+
+    return "-";
+  };
+
+  const handleSimulate = () => {};
+
   return (
     <div className="flex flex-row gap-4 w-full h-full">
       <div className="flex flex-col gap-6 w-[20%] max-w-[240px]">
         <Card className="p-4 h-fit ">
-          <div className="flex flex-row justify-between mb-4">
-            <h3 className="text-[#353535] font-medium">Data</h3>
-            <TruckIcon />
-          </div>
-          <DateSelector onDateSelect={handleDateSelect} isLoading={isLoading} />
-        </Card>
-        <Card className="p-4 h-fit ">
           <CardHeader className="p-0 pb-2 flex-col items-start">
-            <Subtitle>Demanda</Subtitle>
+            <Subtitle>Upload</Subtitle>
           </CardHeader>
           <CardBody className="overflow-visible !p-0 !pt-2">
             <Dropzone
@@ -125,6 +151,69 @@ const PlanningPage: React.FC = () => {
               hasWSS={false}
             />
           </CardBody>
+          <button
+            className={`px-2 py-1 mt-3 rounded-md ${
+              buttonDisabled
+                ? "bg-gray-500 text-gray-400 cursor-not-allowed opacity-50"
+                : "bg-blue-900 text-white"
+            } text-sm font-medium mt-2`}
+            onClick={() => (buttonDisabled ? () => {} : handleGetInformation())}
+          >
+            Enviar
+          </button>
+          <span className="text-xs mt-3 text-gray-400">
+            Ultimo upload:{" "}
+            <span className="text-xs text-black">{handleGetDataFormat()}</span>
+          </span>
+          <span className="text-xs mt-2 text-gray-400">
+            Range:{" "}
+            <span className="text-xs text-black">{handleGetDataFormat()}</span>
+          </span>
+        </Card>
+        <Card className="p-4 h-fit ">
+          <CardBody className="overflow-visible !p-0 !pt-2">
+            <div className="flex justify-between items-center">
+              <p className="text-sm">Backlog Primario</p>
+              <Switch
+                isSelected={backlogPrimary}
+                onValueChange={setBacklogPrimary}
+                size="sm"
+                defaultSelected
+                aria-label="Backlog Primario"
+                classNames={{
+                  wrapper: ["group-data-[selected=true]:bg-blue-900"],
+                }}
+              />
+            </div>
+            <div className="flex justify-between items-center mt-3">
+              <p className="text-sm">Stage (n de caixas)</p>
+              <input
+                type="number"
+                placeholder="3º"
+                value={"0"}
+                onChange={(e) => console.log(e.target.value)}
+                min={0}
+                className="w-12 border-1 border-solid border-gray-300 rounded-md p-1 text-center h-6"
+              />
+            </div>
+            <div>
+              <h2>Aero</h2>
+            </div>
+          </CardBody>
+          <span className="text-xs mt-2 text-gray-400">
+            Produtividade atualizadas:{" "}
+            <span className="text-xs text-black">{handleGetDataFormat()}</span>
+          </span>
+          <button
+            className={`px-2 py-1 mt-3 rounded-md ${
+              buttonDisabled
+                ? "bg-gray-500 text-gray-400 cursor-not-allowed opacity-50"
+                : "bg-blue-900 text-white"
+            } text-sm font-medium mt-2`}
+            onClick={() => (buttonDisabled ? () => {} : handleSimulate())}
+          >
+            Simular
+          </button>
         </Card>
       </div>
 
